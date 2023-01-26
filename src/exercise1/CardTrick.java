6313ce423c3ead7a7018bcaa213ed343ca746950
nithin-ten <122823527+nithin-ten@users.noreply.github.com>
2023-01-26 01:58:10 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -87,18 +87,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Nithin Pinky Tenson");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("My career ambitions: To become Programmer");
+        System.out.println("Be more active on LinkedIn");
+        System.out.println("Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
+        System.out.println("My hobbies: ");
+        System.out.println("-- Music");
+        System.out.println("-- gaming");
+        System.out.println("-- Reading/Watching Art");
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
